231e057d564ba894997ca1a974e67e0be6e0fe82
kulturman <arnaudbakyono@gmail.com>
2022-08-24 11:51:30 +0000

    Update environnement.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a401dbecfdbcb4ccaa149da4b9b20778-552995754.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
